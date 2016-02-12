0af2115a8f29e235956b6c4653c13605689d8e23
Martin Hammerchmidt <martin.hammerchmidt@gmail.com>
2016-02-12 01:21:49 +0100

    fix

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,16 +1,19 @@
 Template.home.onRendered(function()
 {
-	SEO.setDefaults(
+	Tracker.autorun(function()
 	{
-		title: orion.dictionary.get('seo.title'),
-		description: orion.dictionary.get('seo.description'),
-		meta: 
+		SEO.setDefaults(
 		{
-			'property="og:type"': 'website',
-			'property="og:title"': orion.dictionary.get('seo.title'),
-			'property="og:description"': orion.dictionary.get('seo.description'),
-			'property="og:url"': FlowRouter.url(''),
-			//'property="og:image"': 'og.png' //OG IMAGE HERE
-		}
-	});	
+			title: orion.dictionary.get('seo.title'),
+			description: orion.dictionary.get('seo.description'),
+			meta: 
+			{
+				'property="og:type"': 'website',
+				'property="og:title"': orion.dictionary.get('seo.title'),
+				'property="og:description"': orion.dictionary.get('seo.description'),
+				'property="og:url"': FlowRouter.url(''),
+				//'property="og:image"': 'og.png' //OG IMAGE HERE
+			}
+		});	
+	});
 });
